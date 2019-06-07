b7b4f82bcb0735b9646b5199a3861723178813d7
alejandro <chilakill9999@gmail.com>
2019-06-06 19:24:42 -0500

    codigo quote

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes">// maxQuotes=10
+        </app-header>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"> </app-quote-grid>
+    </div>
+</template>
+
+
+<script>
+    import Header from './components/Header.vue';
+    import QuotesGrid from './components/QuoteGrid.vue';
+    export default {
+
+        components:{
+            appHeader: Header,
+            appQuoteGrid: QuotesGrid
+        },
+    
+        data: function(){
+            return{
+                quotes: [
+                    'hola'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods:{
+        deleteQuote(index){
+            this.quotes.splice(index,1);
+        }
+    }
+
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/Quotes.vue
@@ -0,0 +1,27 @@
+<template> 
+    <div class="col-sm6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body quotes">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+    .quotes{
+        cursor: pointer;
+    }
+    .quotes:hover {
+        background-color: #ffe2e2;
+    }
+    .panel-body{
+        font-family:'Arizonia', cursive;
+        font-size: 24px;
+        color:blue;
+    }
+</style>
+
